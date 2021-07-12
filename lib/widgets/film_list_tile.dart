739a2c0ec1c1feb7f227a6744c239ed7533e8e69
Saleem <saleem@flatcircle.io>
2021-07-12 14:29:11 +0200

    restyled list

--- lib/widgets/film_list_tile.dart
+++ lib/widgets/film_list_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:starwars_app/models/film.dart';
 import 'package:starwars_app/services/films_service.dart';
 
@@ -6,13 +7,14 @@ class FilmListTile extends StatelessWidget {
   final ValueChanged<Film> onFilmSelect;
   final filmService = FilmsService();
 
-  FilmListTile({
-    Key? key,
-    required this.film,
-    required this.onFilmSelect
-  }) : super(key: key);
+  FilmListTile({Key? key, required this.film, required this.onFilmSelect})
+      : super(key: key);
 
   final Film film;
+
+  String get dateString {
+    return DateFormat("yyyy-MM-dd").format(film.releaseDate).toString();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,47 +22,64 @@ class FilmListTile extends StatelessWidget {
       onTap: () {
         onFilmSelect(film);
       },
-      leading: CircleAvatar(
-        child: Image(image: filmService.getPosterForFilm(film)),
-        radius: 40,
-      ),
-      title: Container(
-        margin: EdgeInsets.all(4),
-        child: Text(
-          film.title,
-          style: TextStyle(fontWeight: FontWeight.bold),
+      title: Card(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 150,
+              child: Image(
+                image: filmService.getPosterForFilm(film),
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            Flexible(
+              fit: FlexFit.tight,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Container(
+                  height: 130,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        film.title,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        dateString,
+                      ),
+                      Spacer(),
+                      Text(
+                        "Director",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 12),
+                      ),
+                      Text(
+                        film.director,
+                        style: TextStyle(fontSize: 12),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "Poducers",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 12),
+                      ),
+                      FittedBox(
+                        fit: BoxFit.scaleDown,
+                        child: Text(
+                          film.producer,
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-      ),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: 4, left: 4),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Director",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text(film.director),
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 4, left: 4),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Poducers",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text(film.producer),
-              ],
-            ),
-          ),
-        ],
       ),
     );
   }
